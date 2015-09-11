e5d4ac0ef5afeb6f0909c9ee7c2156e2f2373b01
Martijn Braam <martijn@brixit.nl>
2015-09-11 10:48:26 +0200

    Now doesn't ask for the repository when you are working in a git managed path

--- github-changelog.py
+++ github-changelog.py
@@ -5,6 +5,7 @@ from argparse import ArgumentParser
 from collections import OrderedDict
 import os
 import xdg.BaseDirectory
+import re
 
 # Resolve the XDG config path
 config_dir = xdg.BaseDirectory.save_config_path('github-changelog')
@@ -28,26 +29,55 @@ config.read(config_file)
 token = config['general']['token']
 github = Github(token)
 
+# Check if we're already in a github repo
+current_path = os.getcwd().split("/")
+repo_path = None
+for length in range(len(current_path), 0, -1):
+    test_path = "/".join(current_path[0:length])
+    possible_git_dir = os.path.join(test_path, ".git")
+    if os.path.isdir(possible_git_dir):
+        repo_path = possible_git_dir
+        break
+
+# Search for a github repository in the git config
+github_repo_id = None
+if repo_path:
+    regex_repo_id = re.compile(r'[:/]([^/]*/[^/]*)\.git')
+    git_config = ConfigParser()
+    git_config.read(os.path.join(repo_path, "config"))
+    for section in git_config.sections():
+        if 'remote' in section:
+            remote_url = git_config[section]['url']
+            github_repo_id = regex_repo_id.search(remote_url)
+            if github_repo_id:
+                github_repo_id = github_repo_id.group(1)
+                break
+
 # Parse the command line arguments
 argparse = ArgumentParser(description="Changelog generator for Github")
 argparse.add_argument("-f", "--format", help="Output format", default="simple", action="store",
                       choices=["simple", "table"])
 argparse.add_argument("-g", "--group-by", help="Group output by github labels, use multiple times for every label",
                       action="append")
-argparse.add_argument("repository", help="The github repository in user/repo format")
+if not github_repo_id:
+    argparse.add_argument("repository", help="The github repository in user/repo format")
 argparse.add_argument("milestone", help="The milestone to create a changelog for", action="store")
 
 args = argparse.parse_args()
 
 # Resolve the milestone argument to a milestone object on github
-repository = github.get_repo(args.repository)
+if github_repo_id:
+    repository_name = github_repo_id
+else:
+    repository_name = args.repository
+repository = github.get_repo(github_repo_id)
 milestone = None
 for m in repository.get_milestones():
     if m.title == args.milestone:
         milestone = m
 
 if not milestone:
-    print("Cannot find milestone {} in repository {}".format(args.milestone, args.repository))
+    print("Cannot find milestone {} in repository {}".format(args.milestone, github_repo_id))
     exit(1)
 
 # Set up grouping
